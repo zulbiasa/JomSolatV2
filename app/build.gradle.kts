3b332cff06b8a2b54b094a96ba0950e183465352
Zulhelmi <101618544+zulbiasa@users.noreply.github.com>
2025-10-01 01:24:45 +0800

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.zulbiasa.jomsolatv2"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
     }
 
